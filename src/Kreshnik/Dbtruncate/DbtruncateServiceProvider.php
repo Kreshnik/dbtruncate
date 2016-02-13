f42a3f9ec9077be1ae85520ca56a3a8507a55cfa
Kreshnik Hasanaj <kreshnik.hasanaj@gmail.com>
2016-02-13 17:52:47 -0500

    Applied fixes from StyleCI

--- src/Kreshnik/Dbtruncate/DbtruncateServiceProvider.php
+++ src/Kreshnik/Dbtruncate/DbtruncateServiceProvider.php
@@ -1,51 +1,50 @@
-<?php namespace Kreshnik\Dbtruncate;
+<?php
+
+namespace Kreshnik\Dbtruncate;
 
 use Illuminate\Support\ServiceProvider;
 
 class DbtruncateServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['db.truncate'] = $this->app->share(function ($app) {
+            return new DatabaseTruncateCommand($app['db']);
+        });
 
-	}
+        $this->commands(
+            'db.truncate'
+        );
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['db.truncate'] = $this->app->share(function ( $app ) {
-			return new DatabaseTruncateCommand($app['db']);
-		});
-
-		$this->commands(
-			'db.truncate'
-		);
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 }
